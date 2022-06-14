8492fec974d634630e6e459cadc9540073f7aebb
Kevin <kevin_vergara.rodriguez@yahoo.es>
2022-06-13 21:51:43 -0500

    DDBB CONECTION

---
+++ app.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from "body-parser";
+import mysql from 'mysql';
+
+const app = express();
+const { API_VERSION, PORT, HOST, USER, PASSWORD, DATABASE } = process.env;
+const connection = mysql.createConnection({
+    host : HOST,
+    user : USER,
+    password : PASSWORD,
+    database : DATABASE
+});
+
+
+connection.connect( ( e )=>{
+    if( e )  
+    console.log(e);
+}) 
+app.use( bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.json() );
+
+
+app.listen( PORT , () =>{
+    console.log( "########################################");
+    console.log( `## SERVER CONNECTED IN PORT ${ PORT } ##`);
+    console.log( "########################################");
+})
