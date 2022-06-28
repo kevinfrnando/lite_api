e46b3009e811271c696034641fa003b13c7f360f
Kevin <kevin_vergara.rodriguez@yahoo.es>
2022-06-28 15:14:32 -0500

    Connection

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ import mysql from 'mysql';
 
 const app = express();
 const { API_VERSION, PORT, HOST, USER, PASSWORD, DATABASE } = process.env;
+
+/* ***************************
+* **** CONECTION TO DDBB ****
+* ***************************/
 const connection = mysql.createConnection({
     host : HOST,
     user : USER,
     password : PASSWORD,
     database : DATABASE
 });
-
-
 connection.connect( ( e )=>{
     if( e )  
     console.log(e);
-}) 
-app.use( bodyParser.urlencoded({ extended: false }));
+}); 
+
+
+app.use( bodyParser.urlencoded({ extended: false }));       
 app.use( bodyParser.json() );
 
 
@@ -25,4 +29,4 @@ app.listen( PORT , () =>{
     console.log( "########################################");
     console.log( `## SERVER CONNECTED IN PORT ${ PORT } ##`);
     console.log( "########################################");
-})
+}) 
